bf5f0870527fb86df361891255d7e24c9104ff1e
Sh44yy <sk5031361@gmail.com>
2026-08-22 11:04:48 +0500

    first commit

---
+++ JS/js_class1/intro/index.js
@@ -0,0 +1,10 @@
+a = 5;
+b = 4
+// console.log(a+b);
+// alert("Hello World");
+// confirm("want to delete");
+// var userInput = prompt("please enter marks");
+// alert("Grade A+");
+
+var userAge = prompt("please enter your age", 18);
+alert("you are eligible");
---
+++ JS/js_class3/operators_datatypes/script.js
@@ -0,0 +1,38 @@
+// Operators: (+,-<*,/,**, %)
+// Operand: Data on which operation perform
+
+
+// == -> Equality
+// === -> Strick Equality -> Check value and data type as well
+
+// Arithematic Operators
+
+let a = 5;
+let b = 3;
+
+console.log(`a+b: ${a+b}`);
+console.log(`a-b: ${a-b}`);
+console.log(`a*b: ${a*b}`);
+console.log(`a/b: ${a/b}`);
+console.log(`a%b: ${a%b}`);
+console.log(`a**b: ${a**b}`);
+
+// Comperison Operators
+console.log(`a>=b: ${a>=b}`);
+console.log(`a<=b: ${a<=b}`);
+console.log(`a!=b: ${a!=b}`);
+
+// Assignment Operators
+console.log(`a==b: ${a==b}`);
+console.log(`a===b: ${a===b}`);
+
+// Check Datatype
+let name = "Shayan";
+console.log(typeof(name));
+
+// Concat
+let fName = "Aqib";
+let lName = "Ali";
+
+console.log(`My Czn full name is: ${fName} ${lName}`);
+
